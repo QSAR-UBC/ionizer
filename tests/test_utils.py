a65441d41dd8dad1179da3f4752c978a4cd8e827
Olivia Di Matteo <dimatteo.olivia@gmail.com>
2024-05-10 07:13:01 -0700

    Fix error

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -16,7 +16,7 @@ from ionizer.utils import (
     tape_to_json,
 )
 
-from test_decompositions import single_qubit_unitaries relative # pylint: disable=wrong-import-order
+from test_decompositions import single_qubit_unitaries  # pylint: disable=wrong-import-order
 
 
 class TestMatrixAngleUtilities:
